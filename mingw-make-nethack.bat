6d1ca7f7d2901a179e02e7023c848c8eee9836b5
Clive Crous <clive@crous.co.za>
2008-07-16 01:39:07 +0200

    Correct gnuwin32 usage

--- mingw-make-nethack.bat
+++ mingw-make-nethack.bat
@@ -1,14 +1,8 @@
-@set GNUWIN_DIR=C:\Program Files\Gnuwin32
+@which bison
+@IF ERRORLEVEL 1 GOTO need_gnuwin
+@which flex
+@if ERRORLEVEL 1 GOTO need_gnuwin
 
-@rem (WoW64 cmd prompt sets PROCESSOR_ARCHITECTURE to x86) [taken from msys-bat --daniel]
-@if not "x%PROCESSOR_ARCHITECTURE%" == "xAMD64" goto _NotX64
-@set GNUWIN_DIR=C:\Program Files (x86)\Gnuwin32
-:_NotX64
-
-@if not exist "%GNUWIN_DIR%\bin\bison.exe" GOTO need_gnuwin
-@if not exist "%GNUWIN_DIR%\bin\flex.exe" GOTO need_gnuwin
-
-PATH=\msys\1.0\mingw\bin;\MinGW\bin\;%GNUWIN_DIR%\bin;%PATH%
 REM ---------- NetHack ----------
 del /S /Q nethack\win\vultures
 mkdir nethack\win\vultures
@@ -29,6 +23,7 @@ goto EOF
 @echo A good place to get them is the gnuwin project
 @echo see http://gnuwin32.sourceforge.net/
 @echo.
-
+@echo Be sure to use the gnuwin32 prompt for correct paths
+@echo.
 
 :EOF
--- mingw-make-slashem.bat
+++ mingw-make-slashem.bat
@@ -1,15 +1,9 @@
-@set GNUWIN_DIR=C:\Program Files\Gnuwin32
+@which bison
+@IF ERRORLEVEL 1 GOTO need_gnuwin
+@which flex
+@if ERRORLEVEL 1 GOTO need_gnuwin
 
-@rem (WoW64 cmd prompt sets PROCESSOR_ARCHITECTURE to x86) [taken from msys-bat --daniel]
-@if not "x%PROCESSOR_ARCHITECTURE%" == "xAMD64" goto _NotX64
-@set GNUWIN_DIR=C:\Program Files (x86)\Gnuwin32
-:_NotX64
-
-@if not exist "%GNUWIN_DIR%\bin\bison.exe" GOTO need_gnuwin
-@if not exist "%GNUWIN_DIR%\bin\flex.exe" GOTO need_gnuwin
-
-PATH=\msys\1.0\mingw\bin;\MinGW\bin\;%GNUWIN_DIR%\bin;%PATH%
-REM ---------- SLASH'EM ----------
+REM ---------- Slash'EM ----------
 del /S /Q slashem\win\vultures
 mkdir slashem\win\vultures
 xcopy /E /-Y vultures\* slashem\win\vultures\
@@ -20,14 +14,16 @@ mingw32-make -f Makefile.gcc spotless
 mingw32-make -f Makefile.gcc all
 cd ..\..
 strip --strip-all slashem\binary\VulturesClaw.exe
-goto EOF
 
+goto EOF
 
 :need_gnuwin
 @echo.
 @echo You need flex and bison to build vultures. 
 @echo A good place to get them is the gnuwin project
 @echo see http://gnuwin32.sourceforge.net/
+@echo.
+@echo Be sure to use the gnuwin32 prompt for correct paths
 @echo.
 
 
