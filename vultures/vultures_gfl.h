dec5f656926c321156d747f326841dd74fc88747
Clive Crous <clive@crous.co.za>
2009-02-18 15:15:58 +0200

    Correct function declaration

--- vultures/vultures_gfl.h
+++ vultures/vultures_gfl.h
@@ -13,7 +13,7 @@ using std::string;
 extern SDL_Surface *vultures_load_surface(char *srcbuf, unsigned int buflen);
 extern SDL_Surface *vultures_load_graphic(string name);
 
-extern void vultures_save_png(SDL_Surface * surface, char* filename, int with_alpha);
+extern void vultures_save_png(SDL_Surface * surface, string filename, int with_alpha);
 
 extern void vultures_save_screenshot(void);
 
